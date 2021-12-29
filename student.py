b7186bd3eae43ae87a34ca39418c3eb33a0f433c
Balazs9 <sempergerbalazs@gmail.com>
2021-12-29 20:37:36 +0000

    test for success and fail

--- student.py
+++ student.py
@@ -1,5 +1,5 @@
 from datetime import date, timedelta
-
+import requests
 
 class Student:
     """
@@ -13,17 +13,24 @@ class Student:
         self.end_date = date.today() + timedelta(days=365)
         self.naughty_list = False
 
-
     @property
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
-
 
     @property
     def email(self):
         return f"{self._first_name.lower()}.{self._last_name.lower()}@gmail.com"
 
-
     def alert_santa(self):
         self.naughty_list = True
 
+    def apply_extension(self, days):
+        self.end_date = self.end_date + timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get(f"https://company.com/course-schedule/{self._last_name}({self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the requests!" 
